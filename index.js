c6f59f81a25ee6a0aa5c5249046dbd9fd0ca71ee
Andrew54757 <andrews54757@gmail.com>
2016-08-21 21:09:37 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict"
+var width = process.stdout.columns
+var filler = function(a,b) {
+a = a.toString()
+for (var i = 0; i < b - a.length + 1; i++) {
+a += " ";
+
+}
+return a
+
+}
+module.exports = {
+  start() {
+  
+  
+  }
+
+
+
+}
