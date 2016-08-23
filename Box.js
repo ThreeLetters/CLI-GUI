4494ae5fcf90cb244e4482356091117ab7fd29a7
Andrew54757 <andrews54757@gmail.com>
2016-08-23 14:52:49 -0400

    Create Box.js

---
+++ Box.js
@@ -0,0 +1,19 @@
+"use strict"
+
+module.exports = class Box {
+constructor(width,height,options,main) {
+this.width = width;
+this.height = height;
+this.options = options;
+this.index = false;
+this.main = main
+this.selected = 0;
+}
+remove() {
+this.main.layers[this.index] = [];
+this.main.reSortLayers()
+}
+
+
+
+}
