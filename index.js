215d3d339f7bfe6c5014794bb3cb47b2e2ae91a5
Andrew54757 <andrews54757@gmail.com>
2016-08-23 15:41:08 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var cligui = new require('./cligui')()
+
+module.exports = {
+list: cligui.list,
+checkList: cligui.checkList,
+prompt: cligui.prompt,
+createInfoBox: cligui.createInfoBox,
+prepare: cligui.prepare
+
+
+}
