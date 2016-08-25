c4705e10fd46c8c59416f5ab3a66f8695ae55cd9
Andrew54757 <andrews54757@gmail.com>
2016-08-25 15:36:59 -0400

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -2,12 +2,43 @@
 
 const c = require('../lib/cligui.js');
 var CliGui = new c()
+function home() {
 var options = [
   {opt: "prompt",description: "Typing interface"},
-  {opt: "checklist",description: ""},
-  {},
-  {}
+  {opt: "checklist",description: "Checklist interface"},
+  {opt: "list",description: "List"},
+  {opt: "Popup",description: "popup box"}
+  ];
+var callbacks = [
+function() {
+  CliGui.prompt("Prompt","Type.",function() {
+   
+    home()
+    return true;
+  })
+  return true;
+},
+function() {
+  CliGui.checkList("Checklist", "Convienient",["optionA","optionB","optionC"],function() {
+    home()
+    return true;
+  })
+  return true
+},
+function() {
+  CliGui.list("list","same as back there",["option1","option2","option3"],function() {
+    home();
+    return true
+  })
+  return true
+},
+function() {
+  CliGui.createInfoBox(20,5,"This is a popup box!")
+  return false
+}
+]
   
-  ]
-CliGui.list("Example", "Choose an option using arrows and enter and see what it does",options );
+CliGui.list("Example", "Choose an option using arrows and enter and see what it does",options,callbacks );
 CliGui.createInfoBox(25,6,"Welcome to the CLI-GUI Tour!");
+}
+home()
