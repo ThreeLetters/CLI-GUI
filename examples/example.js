e3caf0e54ae4fcacbd904bebcaa0790f312fc9e0
Andrew54757 <andrews54757@gmail.com>
2016-08-25 15:20:55 -0400

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -3,9 +3,11 @@
 const c = require('../lib/cligui.js');
 var CliGui = new c()
 var options = [
-  
-  
+  {opt: "prompt",description: "Typing interface"},
+  {opt: "checklist",description: ""},
+  {},
+  {}
   
   ]
-CliGui.list("Example", "Choose an option using arrows and enter and see what it does",options )
-CliGui.createInfoBox(25,6,"Welcome to the CLI-GUI Tour!")
+CliGui.list("Example", "Choose an option using arrows and enter and see what it does",options );
+CliGui.createInfoBox(25,6,"Welcome to the CLI-GUI Tour!");
