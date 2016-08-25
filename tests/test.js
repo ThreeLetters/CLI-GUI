c497ac71db4c06da35fd87ca30940cafc512b232
Andrew54757 <andrews54757@gmail.com>
2016-08-25 12:45:41 -0500

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 "use strict"
 
-const CliGui = require('../lib/cligui.js');
-
+const c = require('../lib/cligui.js');
+var CliGui = new c()
 CliGui.prompt("test title", "prompt test", function(a){throw a})
