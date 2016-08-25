4c860d3e42a94e6566da82fa0264ccf931eecd66
Andrew54757 <andrews54757@gmail.com>
2016-08-25 12:23:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 
-var c = require('./lib/cligui');
-var cligui = new c()
-
-module.exports = {
-list: cligui.list,
-checkList: cligui.checkList,
-prompt: cligui.prompt,
-createInfoBox: cligui.createInfoBox,
-prepare: cligui.prepare
-
-
-}
