f8b1e3765f5108e7bd4ce26b8e87c6dc7f5800e1
Andrew54757 <andrews54757@gmail.com>
2016-08-25 14:29:59 -0400

    Update and rename tests/test.js to examples/example.js

---
+++ examples/example.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const c = require('../lib/cligui.js');
+var CliGui = new c()
+var options = [
+  
+  
+  
+  ]
+CliGui.list("Example", "Choose an option using arrows and enter and see what it does",options )
--- tests/test.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-
-const c = require('../lib/cligui.js');
-var CliGui = new c()
-CliGui.prompt("test title", "prompt test", function(a){throw a})
