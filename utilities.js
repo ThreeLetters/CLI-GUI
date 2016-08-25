d26ac4e6da9e77800a19bf8ac4d7bd3f08354496
Andrew54757 <andrews54757@gmail.com>
2016-08-24 21:09:32 -0400

    Create utilities.js

---
+++ utilities.js
@@ -0,0 +1,34 @@
+module.exports = class Utilities {
+constructor(main) {
+this.main = main
+
+}
+centerHor(a,g,k) {
+if (!g) g = this.main.width
+if (!k) k = a.length
+var f = Math.abs(a.length - k)
+var b = (g - k - 1) / 2
+var c = "";
+for (var i = 0; i < b; i++) {
+c += " ";
+}
+c += a;
+return this.main.fill(c,g,c.length - f)
+}
+ fill(a,b,k) {
+a = a.toString()
+if (!k) k = a.length
+var c = b - k
+for (var i = 0; i < c; i++) {
+a += " ";
+}
+return a
+}
+wrap(string,maxlen) {
+var results = [];
+while (0==0) {
+if (string.length < maxlen) {
+results.push(string);
+break;
+}
+}
