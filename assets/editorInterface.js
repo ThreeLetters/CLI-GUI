55d3f26a6ba0659346c4bcdfeff816b81f751b3b
Andrew54757 <andrews54757@gmail.com>
2016-09-02 15:17:23 -0400

    Update editorInterface.js

--- assets/editorInterface.js
+++ assets/editorInterface.js
@@ -1,12 +1,13 @@
 "use strict";
 
 module.exports = class EditorInterface {
-constructor(main,out,callback,width,height) {
+constructor(main,out,callback,message,width,height) {
 this.main = main
 this.in = out;
 this.callback = callback;
 this.width = (width) ? width : this.main.width;
 this.height = (height) ? height : this.main.height;
+this.message = message;
 this.init()
 this.result = [];
 this.cursor = {
@@ -15,9 +16,19 @@ this.cursor = {
 }
 this.data = [];
 }
+fill(a,b,c) {
+  return this.main.fill(a,b,c);
+}
+centerHor(a,b,c) {
+  return this.main.centerHor(a,b,c);
+}
 update() {
-  
-  
+  var curr = 0;
+  this.result[curr] = this.centerHor(this.message + " press Esc to exit",this.width)
+  curr ++;
+  for (var i = 0; i < this.height - 2) {
+    
+  }
 }
 init() {
   this.data = this.in.split("\n");
