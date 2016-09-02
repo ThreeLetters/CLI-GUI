f515b4cd03bd713c70715cbe769be0f1b855a152
Andrew54757 <andrews54757@gmail.com>
2016-09-02 14:15:50 -0400

    Update editorInterface.js

--- assets/editorInterface.js
+++ assets/editorInterface.js
@@ -1,9 +1,21 @@
 "use strict";
 
 module.exports = class EditorInterface {
-constructor(main,out,callback) {
+constructor(main,out,callback,width,height) {
 this.main = main
-
+this.in = out;
+this.callback = callback;
+this.width = (width) ? width : this.main.width;
+this.height = (height) ? height : this.main.height;
+this.init()
+this.result = [];
+this.cursor = {
+  x: 0,
+  y: 0
+}
+}
+init() {
+  
 }
 
 
